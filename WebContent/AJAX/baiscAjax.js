cee463c92c44f073475f337750814afcbcef8ef7
singhnavin19 <singhnavin19290@gmail.com>
2026-01-09 20:53:44 +0530

    first commit

--- WebContent/AJAX/baiscAjax.js
+++
@@ -1,7 +0,0 @@
-console.log("I am basic AJAX Example")
-setTimeout(()=>{
-    for (let i=0;i<=10;i++){
-    console.log("Mad hu mai "+i);
-    }  
-},100);
-console.log("I am done")
--- WebContent/this/thisWithNewFunctionCall.js
+++
@@ -1,7 +0,0 @@
-function foo(){
-    this.baz="baz";
-    console.log(this.bar+" "+baz);
-}
-var bar="bar";
-foo();
-// var baz=new foo();
