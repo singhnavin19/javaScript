3fcea22c7310cf8d3a3d49a6ec62882e9d7cd439
singhnavin19 <singhnavin19290@gmail.com>
2026-01-11 14:26:44 +0530

    functiona

---
+++ basic/Functiona.js
@@ -0,0 +1,16 @@
+//1. Declaration
+function sayHello(){
+    console.log("hello")
+}
+sayHello()
+//2. function expression 
+let sayH=function sayByeBye(name1){
+    console.log("ByeBye"+name1)
+}
+sayH()
+
+//3.by using arrow function
+ let sayHArrow=(name1)=>{
+    console.log("ByeBye"+name1)
+}
+sayHArrow("navin")
