6f84c78447d4fa610541e397637a51dc05aade68
singhnavin19 <singhnavin19290@gmail.com>
2026-01-11 15:04:40 +0530

    loops

---
+++ loops/OtherLoops.js
@@ -0,0 +1,12 @@
+// while
+let i=1;
+while(i<=10){
+    console.log(i)
+    i++;
+}
+
+do{
+    console.log(i);
+    i++;
+}while(i<=20);
+
---
+++ loops/forLoopExample.js
@@ -0,0 +1,11 @@
+let i=8;
+for(i=0;i<=10;i++){
+    console.log("Navin "+i)
+}
+console.log(i)
+
+let j=8;
+for(let j=0;j<=10;j++){
+    console.log("Pravin "+j)
+}
+console.log(j)
